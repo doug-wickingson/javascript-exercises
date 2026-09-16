8c7edf93e4066e44cd527061f78225a25deaa843
doug-wickingson <124466501+doug-wickingson@users.noreply.github.com>
2026-09-16 07:15:06 -0500

    Completed 05_leapYears exercise
    Completed this using if and && statements to
    compare logic operators to meet the qualifications
    for a leap year.

--- foundations/loops_and_arrays/05_leapYears/leapYears.js
+++ foundations/loops_and_arrays/05_leapYears/leapYears.js
@@ -1,6 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+	// divisible by 4
+	if (year % 4 === 0) {
+		return true;
+	}
+	// divisible by 100 not leap year
+		// unless also divisible by 400
+	if (year % 100 === 0 && year % 400 === 0) {
+		return true;
+	}
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
