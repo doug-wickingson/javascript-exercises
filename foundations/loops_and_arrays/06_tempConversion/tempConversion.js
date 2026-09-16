dde39393d9fcbc6498b7cd50b4d77240aa26a74a
doug-wickingson <124466501+doug-wickingson@users.noreply.github.com>
2026-09-16 07:48:50 -0500

    Completed 06_tempConversion exercise
    Completed by correctly converting celsius to fahr-
    enheit and fahrenheit to celsius.

--- foundations/loops_and_arrays/06_tempConversion/tempConversion.js
+++ foundations/loops_and_arrays/06_tempConversion/tempConversion.js
@@ -1,8 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+	const celsius = Math.round((fahrenheit - 32) * (5/9)); 
+	return celsius; 
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+	const fahrenheit = Math.round((celsius * 1.8) + 32);
+	return fahrenheit;
 };
+
 
 // Do not edit below this line
 module.exports = {
