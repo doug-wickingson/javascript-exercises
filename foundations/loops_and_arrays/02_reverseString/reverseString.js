979231d594860be44a2f566c7d32fc72d0538c90
doug-wickingson <124466501+doug-wickingson@users.noreply.github.com>
2026-09-16 04:21:44 -0500

    Completed 02_reverseString exercise
    Utilized a for loop to print the string from the
    end of the string provided and work my way back
    to the start.

--- foundations/loops_and_arrays/02_reverseString/reverseString.js
+++ foundations/loops_and_arrays/02_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+	let reversed = '';
+	for (let i = str.length; i > 0; i--) {
+		reversed += str[i - 1];
+	}
+	return reversed;
 };
 
 // Do not edit below this line
