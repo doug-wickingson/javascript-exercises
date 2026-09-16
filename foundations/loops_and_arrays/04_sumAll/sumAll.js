1e8c2a438059deb52ab53156b214675aef74dfbd
doug-wickingson <124466501+doug-wickingson@users.noreply.github.com>
2026-09-16 06:57:08 -0500

    Completed 04_sumAll exercise
    Completed this problem by using a for loop across
    the range and to sum each item up to get the total.

--- foundations/loops_and_arrays/04_sumAll/sumAll.js
+++ foundations/loops_and_arrays/04_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+	sum = 0;
+	
+	if (b > a && b > 0 && a > 0) {
+		for (let i = 0; i <= b - a; i++) {
+			sum += a + i;
+		}
+		
+		return sum;	
+	}
 
+	return "ERROR";
 };
 
 // Do not edit below this line
